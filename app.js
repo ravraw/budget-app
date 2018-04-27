81d4c8554052681272e9918342e57f823205b732
ravindra rawat <ravinrawat.wd@gmail.com>
2018-04-27 03:44:51 +0000

    DOMstring added

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
+////////// DATA CONTROLLER
+
 const budgetController = (function() {
   //code
 })();
 
+////////// UI CONTROLLER
+
 const UIcontroller = (function() {
   // code
+
+  const DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputButton: ".add__btn"
+  };
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
 })();
 
+//////////// GLOBAL APP CONTROLLER
+
 const controller = (function(budgetctrl, UIctrl) {
-  var ctrlAddItem = function() {
+  const DOM = UIctrl.getDOMstrings();
+
+  const ctrlAddItem = function() {
     //code
-    console.log("enetered");
+    // get inputfields values
+    const input = UIctrl.getInput();
+    console.log(input);
   };
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document
+    .querySelector(DOM.inputButton)
+    .addEventListener("click", ctrlAddItem);
 
   document.addEventListener("keypress", function(event) {
     if (event.keyCode === 13 || event.which === 13) {
